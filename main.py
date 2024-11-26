ee9dad99f90fe96fc55ca5ee035deb58cca60c77
Darsh <darshb831@gmail.com>
2024-11-26 09:55:28 -0700

    Fixed Graphing and clean up

--- main.py
+++ main.py
@@ -3,31 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 print('imports success')
 Orignial_Data=pd.read_csv('Drug_Consumption.csv')
- #drug varaibles
-ID = 0
-Age = 1
-Gender = 2
-Education = 3
-Country = 4
-Ethnicity = 5
-Alcohol = 13
-Amphet = 14
-Amyl = 15
-Benzos = 17
-Caffeine = 18
-Cannabis = 19
-Chocolate = 20
-Coke = 21
-Crack = 22
-Ecstasy = 23
-Heroin = 24
-Ketamine = 25
-Legalh = 26
-LSD = 27
-Meth = 28
-Mushroom = 29
-Nicotine = 30
-VSA = 32
+
 
 Drugs = ['Alcohol','Amphet','Amyl','Benzos','Caff','Cannabis','Choc','Coke','Crack','Ecstasy','Heroin','Ketamine','Legalh','LSD','Meth','Mushrooms','Nicotine','Semer','VSA']
 plt.show()
@@ -52,7 +28,7 @@ print('line 49')
 Education_Drug = [['Doctorate degree',0,0,0],
                   ['Masters degree',0,0,0],
                   ['University degree',0,0,0],
-                  ['Professional certificate/diploma',0,0,0],
+                  ['Professional certificate/ diploma',0,0,0],
                   ['Some college or university, no certificate or degree',0,0,0],
                   ['Left school at 18 years',0,0,0],
                   ['Left school at 17 years',0,0,0],
@@ -62,18 +38,19 @@ Education_Drug = [['Doctorate degree',0,0,0],
 print('line 60')
 for i in range(len(Education_Drug)):
   education_level = Education_Drug[i][0]
-  Drug = 'Heroin'
+  Drug = 'Coke'
   for j in range(len(Modified_Data)):
     if Modified_Data.loc[j,'Education'] == education_level:
       Education_Drug[i][1] += Modified_Data.loc[j,Drug]
       Education_Drug[i][2] += 1
   Education_Drug[i][3] = Education_Drug[i][1]/Education_Drug[i][2]
+print(Education_Drug)
 
-plt.bar(Education_Drug[0], Education_Drug[3])
+x_value = []
+y_value =[]
+for i in range(len(Education_Drug)):
+  x_value.append(Education_Drug[i][0])
+  y_value.append(Education_Drug[i][3])
+
+plt.bar(x_value, y_value)
 plt.show()
-input()
-
-
-#for index, row in Modified_Data.iterrows()
-
-
